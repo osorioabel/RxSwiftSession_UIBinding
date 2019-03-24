e4acd0fb68b36dcdb096f8344e938cff9f83cf5c
Abel Osorio <abel.osorio@strv.com>
2019-03-24 18:43:51 +0100

    [feat]: Add xcodeproj and pods

---
+++ UIBinding/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  UIBinding
+//
+//  Created by Abel Osorio on 3/24/19.
+//  Copyright © 2019 Abel Osorio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
